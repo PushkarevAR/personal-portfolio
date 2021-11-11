aff959d7695094342c3d3ff9e7a875fba42c39ff
PushkarevAR <pm.pushkarev.ar@gmail.com>
2021-11-11 15:11:09 +0300

    init

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./header.module.scss";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <li>
+          <ul>
+            <a href="#"></a>Home
+          </ul>
+          <ul>
+            <a href="#"></a>About
+          </ul>
+          <ul>
+            <a href="#"></a>Skills
+          </ul>
+          <ul>
+            <a href="#">Education</a>
+          </ul>
+          <ul>
+            <a href="#">GFX</a>
+          </ul>
+        </li>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/section/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Home() {
+  return (
+    <section>
+      <h3>U r wellcome!</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid
+        exercitationem labore accusamus aspernatur doloribus quidem facere saepe
+        distinctio eum eius ab autem sunt obcaecati, ipsam id? Vitae eum
+        expedita nisi!
+      </p>
+    </section>
+  );
+}
+
+export default Home;
