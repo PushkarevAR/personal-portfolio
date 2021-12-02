9583a593bfa5591502cc89071ac05c70847461fc
PushkarevAR <pm.pushkarev.ar@gmail.com>
2021-12-02 17:01:57 +0300

    active state nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,42 @@
-import React from "react";
-import "./Header.module.scss";
+import React, { useState } from "react";
+import styles from "./Header.module.scss";
 import ButtonScrollTo from "../button/ButtonScrollTo";
 
 function Header() {
+  const [isActive, setActive] = useState({
+    'home': true,
+    'about': false,
+    'skills': false,
+    'education': false,
+    'gfx': false,
+  });
+
+  const deactivate = () => Object.keys(isActive).map(key => isActive[key] = false);
+
+  const clickHandler = (e) => {
+    const current = e.target.textContent.toLowerCase().replace(/\s/g, '');
+    const deactivateState = deactivate();
+    setActive({...deactivateState, [current]: true});
+  };
+
   return (
     <header>
       <nav>
         <li>
-          <ul>
+          <ul className={isActive.home ? styles.active : ""} onClick={clickHandler}>
             <ButtonScrollTo toId="home"> Home </ButtonScrollTo>
-            {/* <a href="#home">Home</a> */}
           </ul>
-          <ul>
+          <ul className={isActive.about ? styles.active : ""} onClick={clickHandler}>
             <ButtonScrollTo toId="about"> About </ButtonScrollTo>
-            {/* <a href="#about">About</a> */}
           </ul>
-          <ul>
+          <ul className={isActive.skills ? styles.active : ""} onClick={clickHandler}>
             <ButtonScrollTo toId="skills"> Skills </ButtonScrollTo>
-            {/* <a href="#skills">Skills</a> */}
           </ul>
-          <ul>
+          <ul className={isActive.education ? styles.active : ""} onClick={clickHandler}>
             <ButtonScrollTo toId="education"> Education </ButtonScrollTo>
-            {/* <a href="#education">Education</a> */}
           </ul>
-          <ul>
+          <ul className={isActive.gfx ? styles.active : ""} onClick={clickHandler}>
             <ButtonScrollTo toId="gfx"> GFX </ButtonScrollTo>
-            {/* <a href="#gfx">GFX</a> */}
           </ul>
         </li>
       </nav>
